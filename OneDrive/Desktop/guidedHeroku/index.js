fde5c6e043c958f731401fd8bd1b6b1348fddbe1
Kristian Rosales <r.kristian40@gmail.com>
2023-04-03 12:33:59 -0500

    test commit

---
+++ OneDrive/Desktop/guidedHeroku/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/hello", (req, res) => {
+  res.json("hello there!");
+});
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
